bf2b2ecb92e28ec84e32e139e9cf05bbb093a85a
David <david.barbalas@gmail.com>
2024-12-03 22:48:05 -0800

    adding debug comments

--- day2.py
+++ day2.py
@@ -10,7 +10,12 @@ with open(input_file, "r") as file:
         l = row.split((" "))
         x = [int(a) for a in l]
 
-        print(x)
+        print(f"Input list is: {x}")
+
+        difference = np.diff(x)
+        print(f"Adjacent diff is: {difference}")
+        sign_diff = np.diff(np.sign(difference)).sum() == 0
+        print("All decreasing or increasing: {sign_diff}")
 
 
 # %%

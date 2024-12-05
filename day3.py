87a16e78ad97efa0dde1c53244474a11d80836e8
david <david>
2024-12-04 22:43:57 -0800

    getting somewhere with the regex search for the don't and dos

--- day3.py
+++ day3.py
@@ -18,3 +18,24 @@ with open(input_file, "r") as file:
             output += int(m[0]) * int(m[1])
 
 print(f"Total output is: {output}")
+# %% part 2
+
+input_file = "input_d3"
+# p = re.compile(r"mul[(](\d+),(\d+)+[)]")
+output = 0
+# with open(input_file, "r") as file:
+#     for row in file.readlines():
+# s = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+p = re.compile(r"(mul[(]\d+,\d+[)])|(don't())")
+
+s = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+matches = p.findall(s)
+print(matches)
+
+# print(matches)
+
+# for m in matches:
+#     output += int(m[0]) * int(m[1])
+
+# print(f"Total output is: {output}")

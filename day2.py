8c1a491b391b65537a97dbfc3606bebd270091e3
david <david>
2024-12-04 21:50:34 -0800

    I am a doofus, we need to check for both conditions for each sublist

--- day2.py
+++ day2.py
@@ -84,17 +84,25 @@ with open(input_file, "r") as file:
 
         damp_sign = check_signs_damp(x)
 
-        if damp_sign[0] != check_signs(x):
-            print(f"Input list is: {x}")
-            print(f"Damp  list is: {damp_sign[1]}")
+        # if damp_sign[0] != check_signs(x):
+        #     print("\nSign Check:")
+        #     print(f"Input list is: {x}")
+        #     print(f"Damp  list is: {damp_sign[1]}")
 
         # print(f"Dampened sign of change is correct: {damp_sign[0] }")
 
-#         damp_gap = check_gap_damp(x)
-#         # print(f"Dampened magnitude of change is correct: {damp_gap}")
+        damp_gap = check_gap_damp(x)
 
-#         if damp_sign[0] and damp_gap[0]:
-#             # print(f"Dampened safety is good ")
-#             counter += 1
+        # if damp_gap[0] != check_gap(x):
+        #     print(f"\nGap Size:")
+        #     print(f"Input list is: {x}")
+        #     print(f"Damp  list is: {damp_gap[1]}")
 
-# print(f"Total number of safe reports is: {counter}")
+        #         # print(f"Dampened magnitude of change is correct: {damp_gap}")
+
+        if damp_sign[0] and damp_gap[0] and not check_signs(x) and not check_gap(x):
+            print(f"Correct list is: {damp_gap[1]}")
+            # print(f"Dampened safety is good ")
+            counter += 1
+
+print(f"Total number of safe reports is: {counter}")

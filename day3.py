fa4725fb7ed0ad673488f279e23dd360882a1436
david <david>
2024-12-04 22:14:03 -0800

    fixing regex typo dropping plus sign on the second numerical term

--- day3.py
+++ day3.py
@@ -2,15 +2,19 @@
 import re
 import numpy
 
-s = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-
-p = re.compile(r"mul[(](\d+),(\d)+[)]")
-matches = p.findall(s)
-
-print(matches)
-
+input_file = "input_d3"
+p = re.compile(r"mul[(](\d+),(\d+)+[)]")
 output = 0
-for m in matches:
-    output += int(m[0]) * int(m[1])
+with open(input_file, "r") as file:
+    for row in file.readlines():
+        # s = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+        s = row
+
+        matches = p.findall(s)
+
+        print(matches)
+
+        for m in matches:
+            output += int(m[0]) * int(m[1])
 
 print(f"Total output is: {output}")

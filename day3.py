fcd8bcd7ef868d2212073b624a78988af8050e9d
david <david>
2024-12-04 22:10:41 -0800

    got the original regex working, now just have it pull the two numbers to multiply

--- day3.py
+++ day3.py
@@ -1,9 +1,14 @@
 # %%
 import re
+import numpy
 
 s = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
-p = re.compile(r"mul[(]\d+,\d+[)]")
-m = p.findall(s)
+p = re.compile(r"mul[(](\d+),(\d)+[)]")
+matches = p.findall(s)
 
-print(m)
+print(matches)
+
+p2 = re.compile(r"\d+,\d+")
+
+# for m in matches:

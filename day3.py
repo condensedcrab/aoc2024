28a6a59bbcb30f9737657e7bddeeb3beaa268b34
david <david>
2024-12-11 08:45:12 -0800

    updated the regex phrase to grab the parantheses for the don't and do commands

--- day3.py
+++ day3.py
@@ -26,16 +26,17 @@ output = 0
 # with open(input_file, "r") as file:
 #     for row in file.readlines():
 # s = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-p = re.compile(r"(mul[(]\d+,\d+[)])|(don't())")
+p = re.compile(r"(mul[(]\d+,\d+[)])|(don't[()])| (do[()])")
 
 s = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 matches = p.findall(s)
 print(matches)
 
-# print(matches)
+# clean up the parsed results
 
-# for m in matches:
-#     output += int(m[0]) * int(m[1])
-
-# print(f"Total output is: {output}")
+clean_match = []
+for m in matches:
+    for phrase in m:
+        if phrase != "":
+            clean_match.append(phrase)

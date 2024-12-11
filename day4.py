30df9c1c8ea02ff5e8fc32da119190f810b9cd60
david <david>
2024-12-11 09:14:02 -0800

    adding placeholders for row/column and diagonal search

--- day4.py
+++ day4.py
@@ -1,4 +1,30 @@
+# %% define functions
 import re
 
+def row_search:
+    
+    return
+
+def col_search:
+    
+    return
+
+def diagonal_search:
+    
+    return
+
+# %%
 
 input_file = "test"
+
+s = ""
+with open(input_file, "r") as file:
+    for row in file.readlines():
+        s += row
+
+print(s)
+
+p = re.compile(r"(?:XMAS)| (?:SAMX)")
+
+m = p.findall(s)
+print(m)

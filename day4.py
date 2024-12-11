ecabfd00148e6593d76a4ceb95f449381e9bb007
david <david>
2024-12-11 09:34:09 -0800

    trying to troubleshoot the diagonal search

--- day4.py
+++ day4.py
@@ -21,7 +21,20 @@ def col_search(s):
 def diagonal_search(s):
     counts = 0
     for idx, val in enumerate(s):
-        if s[idx : idx + 40 : 10] in ["XMAS", "SAMX"]:
+
+        # check if downward diagonal fits within row and column borders
+        # if divmod(idx, 10)[0] >= divmod(len(s), 10)[0] - 3:
+        #     continue
+        # if divmod(idx, 10)[1] > 6:
+        #     continue
+        if idx > 50:
+            break
+        test_str = val + s[idx + 11] + s[idx + 12] + s[idx + 13]
+        if r"\n" in test_str:
+            continue
+        print(test_str)
+
+        if test_str in ["XMAS", "SAMX"]:
             counts += 1
     return counts
 

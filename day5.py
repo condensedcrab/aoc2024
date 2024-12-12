97f3d667137ddb070237812c04107569dea49cc8
David <david.barbalas@gmail.com>
2024-12-12 11:57:50 -0800

    cooking up logic for dictionary check

--- day5.py
+++ day5.py
@@ -14,6 +14,22 @@ class day5:
                     
         print(self.rules)
         print(self.updates)
+        
+    def verify_updates(self):
+        
+        for update in self.updates:
+            splits = update.split(",")
+            
+            for s in splits:
+                r = self.rules.get('s')
+                if r is None:
+                    continue
+                else:
+                    if update.find(r) < update.find(s):
+                        continue
+                    else:
+                        print(f"Rule satisfied: {s} is before {r} in update: {update}")
+
 
 if __name__ == '__main__':
     d = day5()

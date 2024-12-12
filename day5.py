057c85f49d1cc3ebca5c5d9efca2f6b97cfbbfd1
David <david.barbalas@gmail.com>
2024-12-12 11:53:12 -0800

    converting the rules to a dictionary for easier parsing

--- day5.py
+++ day5.py
@@ -2,12 +2,13 @@ class day5:
 
     def __init__(self):
 
-        self.rules = []
+        self.rules = {}
         self.updates = []
         with open('test', 'r') as f:
             for line in f.readlines():
                 if "|" in line:
-                    self.rules.append(line.strip("\n"))
+                    temp_str = line.strip("\n").split('|')
+                    self.rules[temp_str[0]] = temp_str[1]
                 elif "," in line:
                     self.updates.append(line.strip("\n"))
                     

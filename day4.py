e580b964d3e76c72f5e63164a5493bb3c0402341
David <david.barbalas@gmail.com>
2024-12-12 08:33:03 -0800

    adding day4 problem input

--- day4.py
+++ day4.py
@@ -14,7 +14,7 @@ def row_search(s):
 
 def col_search(s):
     counts = 0
-    row_size = 11
+    row_size = s.find('\n')+1
     for idx, val in enumerate(s):
         if s[idx : idx + 4*row_size : row_size] in ["XMAS", "SAMX"]:
             counts += 1
@@ -25,7 +25,7 @@ def col_search(s):
 
 def diagonal_search(s):
     counts = 0
-    row_size = 11
+    row_size = s.find('\n')+1
     for idx, val in enumerate(s):
 
         # check that we don't overrun the number of rows
@@ -40,18 +40,20 @@ def diagonal_search(s):
         if test_str in ["XMAS", "SAMX"]:
             counts += 1
         
-        c = counts
-        print(f"Diag found: {counts}")
         # construct antidiagonal
         test_str = ""
         for i in range(0, 4):
             test_str += s[idx + i * (row_size + 1)]
+            
+        if test_str in ["XMAS", "SAMX"]:
+            counts += 1
 
-        print(f"Anti-diag found: {counts-c}")
-    
+
+    print(f"Diagonals found: {counts}")
     return counts
 
 
+print(f"\n\n\nTest Case: ")
 input_file = "test"
 
 s = ""
@@ -65,4 +67,18 @@ total_counts = (
 )
 
 print(f"Total instances of XMAS found: {total_counts}")
-# format is 9 letters with a '\n' char
+
+print(f"\n\n\nInput File: ")
+input_file = "input_d4"
+
+s = ""
+with open(input_file, "r") as file:
+    for row in file.readlines():
+        s += row
+
+# print(s)
+total_counts = (
+    row_search(s) + col_search(s) + diagonal_search(s)
+)
+
+print(f"Total instances of XMAS found: {total_counts}")

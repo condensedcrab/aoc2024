80cfc542f6d5a543c1e52fc297249845db70639f
David <david.barbalas@gmail.com>
2024-12-12 08:58:26 -0800

    implemeting the x-mas search. Need both the diagonal and antidiagonal to be hits for a count.

--- day4.py
+++ day4.py
@@ -52,6 +52,26 @@ def diagonal_search(s):
     print(f"Diagonals found: {counts}")
     return counts
 
+def xmas_search(s):
+    counts = 0
+    row_size = s.find('\n')+1
+    
+    for idx, val in enumerate(s):
+        # check that we don't overrun the number of rows
+        if idx + 2*(row_size + 1) >= len(s) or idx + 2*(row_size - 1) < 0:
+            continue
+        
+        test_str = ["",""]
+        for i in range(0, 3):
+            test_str[0] += s[idx + i * (row_size + 1)]
+            test_str[1] += s[idx + i * (row_size - 1)]
+            
+        if test_str[0] in ["MAS", "SAM"] and test_str[1] in ["MAS", "SAM"]:
+            counts += 1
+        
+    print(f"X-mases found: {counts}")
+    return counts
+
 
 print(f"\n\n\nTest Case: ")
 input_file = "test"
@@ -81,4 +101,7 @@ total_counts = (
     row_search(s) + col_search(s) + diagonal_search(s)
 )
 
+xmas_counts = xmas_search(s)
 print(f"Total instances of XMAS found: {total_counts}")
+print(f"Total X-Mases found: {xmas_counts}")
+

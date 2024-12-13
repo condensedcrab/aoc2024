6cc6b057cc2357fd18ab3c7c7443680b6921f548
David <david.barbalas@gmail.com>
2024-12-12 20:25:26 -0800

    got the guard finding logic working

--- day6.py
+++ day6.py
@@ -8,11 +8,18 @@ class puzzle:
             for line in f.readlines():
                 self.input.append(list(line.replace("\n","")))
                 
+    def find_guard(self):
+        for row_idx,row in enumerate(self.input):
+            try:
+                col_idx = row.index("^")
+                break
+            except:
+                pass
+            
+        self.guard_loc = [row_idx,col_idx]
+        return 
       
 if __name__ == '__main__':
     p = puzzle()
     p.parse_input()
-
-    
-    print("Day 6: ")
-    print(p.input)
+    p.find_guard()

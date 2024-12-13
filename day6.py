e0a3bdacef59e630e121cffbe4c13b65f88279e3
david <david>
2024-12-13 08:22:12 -0800

    refactoring division of guard location and how to do the movement logic

--- day6.py
+++ day6.py
@@ -2,34 +2,32 @@ class puzzle:
 
     def __init__(self):
         self.map = []
-        self.guard_present = True
         self.guard_loc = []
 
     def parse_input(self):
-        with open('test', 'r') as f:
+        with open("test", "r") as f:
             for line in f.readlines():
-                self.map.append(list(line.replace("\n","")))
-                
-    def find_guard(self):
+                self.map.append(list(line.replace("\n", "")))
+
+    def find_guard_start(self):
         col_idx = -1
-        for row_idx,row in enumerate(self.map):
+        for row_idx, row in enumerate(self.map):
             try:
                 col_idx = row.index("^")
                 break
             except:
                 pass
-        
+
         if col_idx > 0:
-            self.guard_loc = [row_idx,col_idx]
-            self.guard_present = True
-        else:
-            self.guard_present = False
-        return 
-      
-if __name__ == '__main__':
+            self.guard_loc = [row_idx, col_idx]
+
+        return
+
+
+if __name__ == "__main__":
     p = puzzle()
     p.parse_input()
     # find current position of guard
     p.find_guard()
-    
-    # 
+
+    #

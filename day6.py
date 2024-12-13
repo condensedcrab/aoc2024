5c9f7d1cbbd41ad4ff4d348f9db88cd84a5bed6e
david <david>
2024-12-13 08:31:02 -0800

    refactoring the guard logic to split out the next step algo into a separate function

--- day6.py
+++ day6.py
@@ -3,6 +3,7 @@ class puzzle:
     def __init__(self):
         self.map = []
         self.guard_loc = []
+        self.guard_positions = []
 
     def parse_input(self):
         with open("test", "r") as f:
@@ -23,11 +24,32 @@ class puzzle:
 
         return
 
+    def guard_logic(self):
+        # initialize state
+        row_idx = self.guard_loc[0]
+        col_idx = self.guard_loc[1]
+        direction = "up"
+
+        for i in range(0, 10):
+            row_idx, col_idx, direction = self.next_step(
+                self.map, row_idx, col_idx, direction
+            )
+
+    def next_step(map, row_idx, col_idx, direction):
+
+        return row_idx, col_idx, direction
+
+        # move in direction
+
 
 if __name__ == "__main__":
     p = puzzle()
     p.parse_input()
     # find current position of guard
-    p.find_guard()
+    p.find_guard_start()
+
+    p.guard_logic()
+
+    print(f"Guard will travel to {len(self.guard_positions)} different positions.")
 
     #

1442a8606b709fee39a6a042a21da879abf6f733
David <david.barbalas@gmail.com>
2024-12-12 20:09:05 -0800

    setting up parser to pull each char as independent list element so we can index by row/column. Removing the new line char "\n"

--- day6.py
+++ day6.py
@@ -6,8 +6,13 @@ class puzzle:
     def parse_input(self):
         with open('test', 'r') as f:
             for line in f.readlines():
-                self.input.append(line)
+                self.input.append(list(line.replace("\n","")))
+                
       
 if __name__ == '__main__':
     p = puzzle()
+    p.parse_input()
+
     
+    print("Day 6: ")
+    print(p.input)

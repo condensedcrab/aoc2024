2da14850e6d61d339ee9191056496efe43e88d97
David <david.barbalas@gmail.com>
2024-12-13 08:08:30 -0800

    fleshing out finding the guard to start the process of calculating the locations

--- day6.py
+++ day6.py
@@ -11,17 +11,25 @@ class puzzle:
                 self.map.append(list(line.replace("\n","")))
                 
     def find_guard(self):
+        col_idx = -1
         for row_idx,row in enumerate(self.map):
             try:
                 col_idx = row.index("^")
                 break
             except:
                 pass
-            
-        self.guard_loc = [row_idx,col_idx]
+        
+        if col_idx > 0:
+            self.guard_loc = [row_idx,col_idx]
+            self.guard_present = True
+        else:
+            self.guard_present = False
         return 
       
 if __name__ == '__main__':
     p = puzzle()
     p.parse_input()
+    # find current position of guard
     p.find_guard()
+    
+    # 

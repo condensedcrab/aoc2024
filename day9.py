7725767da7433cbaf4c8115ab8d6e9506dfcf57b
David <david.barbalas@gmail.com>
2024-12-20 00:28:31 -0800

    got the number/dot movement completed

--- day9.py
+++ day9.py
@@ -1,5 +1,5 @@
 s = "2333133121414131402"
-s = "12345"
+# s = "12345"
 
 
 def unwrap(s):
@@ -14,7 +14,43 @@ def unwrap(s):
             for n in range(0, int(val)):
                 output.append(".")
 
-    return "".join(output)
+    return output
 
 
-print(unwrap(s))
+def first_dot(my_str):
+    return my_str.index(".")
+
+
+def last_digit(my_str):
+    first_digit = 0
+    for idx, val in enumerate(my_str):
+        if val.isdigit() and idx > first_digit:
+            first_digit = idx
+
+    return first_digit
+
+
+def move_files(my_str):
+    while first_dot(my_str) < last_digit(my_str):
+        idx_dot = first_dot(my_str)
+        idx_num = last_digit(my_str)
+
+        value = my_str[idx_num]
+        my_str[idx_num] = "."
+        my_str[idx_dot] = value
+
+        print(my_str)
+
+    return my_str
+
+
+def calc_checksum(data):
+    output = []
+
+    return output
+
+
+a = unwrap(s)
+b = move_files(a)
+
+print(b)
